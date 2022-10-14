5e84295c2ff11859252aaeec206bb4e31e4a9233
Boxi Liu <liub92@mcmaster.ca>
2022-10-14 13:06:55 -0400

    create texts and buttons

--- kivy/main.py
+++ kivy/main.py
@@ -1,11 +1,32 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+        self.inside = GridLayout()
+        self.inside.cols = 2
+        self.inside.add_widget(Label(text="first_name"))
+        self.text_input = TextInput(multiline=False)
+        self.inside.add_widget(self.text_input)
+        self.add_widget(self.inside)
+        self.submit = Button(text="Submit", font_size=14)
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self, instance):
+        print("Pressed!\n")
+        self.text_input.text = ""
 
 class MyApp(App):
     def build(self):
-        return Label(text="Boxi")
+        return MyGrid()
 
 
 if __name__ == '__main__':
